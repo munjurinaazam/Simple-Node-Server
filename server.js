80ef55fedfb0587862394891586614ceca404c68
Munjurina Azam <1521880@iub.edu.bd>
2018-07-18 03:30:52 +0000

    ClassWork 7

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*classwork 6
 var http = require('http');
 var express =require('express');
 var app =express();
@@ -5,6 +6,30 @@ var server =http.Server(app);
 
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/index.html');
+});
+app.get('/system/about',function(req,res){
+  res.sendFile(__dirname+'/about.html');
+});
+  server.listen(process.env.PORT, process.env.IP, function(){
+    console.log('Server running');
+  });
+*/
+var http = require('http');
+var express =require('express');
+var app =express();
+var bodyParser =require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+var server =http.Server(app);
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
+app.get('/form',function(req,res){
+  res.sendFile(__dirname+'/form.html');
+});
+app.post('/submit_user',function(req,res){
+ console.log(req.body);
 });
 app.get('/system/about',function(req,res){
   res.sendFile(__dirname+'/about.html');

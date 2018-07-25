8bf39de0700b25a4823fafe69ca7f1d28491c2f0
Munjurina Azam <1521880@iub.edu.bd>
2018-07-25 03:32:58 +0000

    classwork8

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get('/system/about',function(req,res){
     console.log('Server running');
   });
 */
+/*//classwork7
 var http = require('http');
 var express =require('express');
 var app =express();
@@ -30,6 +31,46 @@ app.get('/form',function(req,res){
 });
 app.post('/submit_user',function(req,res){
  console.log(req.body);
+});
+app.get('/system/about',function(req,res){
+  res.sendFile(__dirname+'/about.html');
+});
+  server.listen(process.env.PORT, process.env.IP, function(){
+    console.log('Server running');
+  });*/
+  //classwork8
+  var http = require('http');
+var express =require('express');
+var app =express();
+var bodyParser =require('body-parser');
+mongo.MongoClient.connect(uri,
+{useNewUrlParser:true},
+  function(err,client){
+    if(err){
+      console.log("could not connect to MongoDB");
+    }else{
+      db= client.db('simple-node');
+    }
+  });
+var save=function (from_data){;
+  db.createCollection('users',function(err,collection){});
+  var collection=db.collection('users');
+  collection.save(from_data);
+  }
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+var server =http.Server(app);
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
+app.get('/form',function(req,res){
+  res.sendFile(__dirname+'/form.html');
+});
+app.post('/submit_user',function(req,res){
+ console.log(req.body);
+ save(req.body);
+ res.status('200');
 });
 app.get('/system/about',function(req,res){
   res.sendFile(__dirname+'/about.html');
